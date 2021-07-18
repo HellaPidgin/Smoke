b32fd4a40ed4b488058732edbca852cc8aac4ebc
Samuel Okoro <samuelokoro@samuels-air.home>
2021-07-18 21:41:33 +0100

    Hello world

---
+++ sketch.js
@@ -0,0 +1,27 @@
+// Testing Snakecase compatitbility
+const FETCH_RECHARGING = null;
+const SMOKE_RECHARGING = null;
+
+// Testing file link
+console.log("Welcome to P5");
+console.log("Hello World!!");
+
+// setting up p5 dependencies
+function setup() {
+    // put setup code here
+    const tacos= [];
+    const burritos= 2;
+    const abogada = "bat";
+    const cheeseCake = 2;
+    }
+
+    // Log exciting render 5 projects.
+    function draw() {
+        ellipse(50, 50, 80, 80);
+        print("connecticut")
+        print("connecticut")
+        print("Seattle")
+        print("connecticut")
+    // put drawing code here
+
+    }
