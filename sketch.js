6bd9a2ad0aa1ef16ab2721b83973586203871b93
Samuel Okoro <samuelokoro@samuels-air.home>
2021-07-26 13:11:50 +0100

    Nuke Mac

--- sketch.js
+++ sketch.js
@@ -1,10 +1,31 @@
 // Testing Snakecase compatitbility
-const FETCH_RECHARGING = null;
+const AURA_RECHARGING = null;
 const SMOKE_RECHARGING = null;
 
 // Testing file link
 console.log("Welcome to P5");
 console.log("Hello World!!");
+
+/* Extended Array */
+var colors = [
+{"name":"Amethyst","hex":"#9b5de5","rgb":[155,93,229],"cmyk":[32,59,0,10],"hsb":[267,59,90],"hsl":[267,72,63],"lab":[52,52,-60]},
+{"name":"Magenta Crayola","hex":"f15bb5","rgb":[241,91,181],"cmyk":[0,62,25,5],"hsb":[324,62,95],"hsl":[324,84,65],"lab":[61,66,-18]},
+{"name":"Minion Yellow","hex":"fee440","rgb":[254,228,64],"cmyk":[0,10,75,0],"hsb":[52,75,100],"hsl":[52,99,62],"lab":[90,-8,78]},
+{"name":"Capri","hex":"00bbf9","rgb":[0,187,249],"cmyk":[100,25,0,2],"hsb":[195,100,98],"hsl":[195,100,49],"lab":[71,-18,-42]},
+{"name":"Sea Green Crayola","hex":"00f5d4","rgb":[0,245,212],"cmyk":[100,0,13,4],"hsb":[172,100,96],"hsl":[172,100,48],"lab":[87,-56,2]}
+]
+
+
+function PROCESS_MESSAGE_INPUT_DATA(){
+
+}
+function PUSH_MESSAGE_DATA_TO_BACKEND(){
+
+}function PUSH_MESSAGE_DATA_TO_BACKEND(){
+
+}
+
+var w = 0;
 
 // setting up p5 dependencies
 function setup() {
@@ -14,20 +35,30 @@ function setup() {
     const burritos= 2;
     const abogada = "bat";
     const cheeseCake = 2;
-    const w = 0;
+    
     const falcons = [];
     }
 
+    const neonCooker = (color1, color2) => {
+
+    }
+
     // Log exciting render 5 projects.
+
     function draw() {
+        background('#fff');
         ellipse(180, 70, 80, 80);
+        fill(colors[4].rgb)
         ellipse(350, 50, 80, 80);
+        fill(colors[1].rgb);
         ellipse(50, 150, 80, 80);
-        ellipse(450, 150, 80, 80);
-        print("connecticut")
-        print("connecticut")
-        print("Seattle")
-        print("Gotham")
-        print("connecticut")
+        fill(colors[2].rgb);
+        ellipse(450 + w, 150, 80, 80);
+        fill(colors[3].rgb);
+
+
+        if(w < 10){
+            w++
+        }
 
     }
